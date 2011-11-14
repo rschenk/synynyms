12e74377c230db64071a95bb8a3e8a5560b3fc98
Ryan Schenk <rschenk@gmail.com>
2011-11-13 19:41:38 -0500

    Messing around with socket.io to make Firefox8 happy

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ io.configure('production', function(){
   io.set('log level', 1);                    // reduce logging
   io.set('transports', [                     // enable all transports (optional if you want flashsocket)
       'websocket'
-    , 'flashsocket'
+//    , 'flashsocket' Firefox 8 is sucking the big one
     , 'htmlfile'
     , 'xhr-polling'
     , 'jsonp-polling'
