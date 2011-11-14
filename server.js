eb8729da319138b04b718971bdb4e073f5d757bf
Ryan Schenk <rschenk@gmail.com>
2011-11-13 21:36:02 -0500

    Nevermind. No workie

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ io.configure('production', function(){
   io.set('log level', 1);                    // reduce logging
   io.set('transports', [                     // enable all transports (optional if you want flashsocket)
       'websocket'
-    , 'flashsocket'
+//    , 'flashsocket' // Firefox 8 and no.de is not working
     , 'htmlfile'
     , 'xhr-polling'
     , 'jsonp-polling'
